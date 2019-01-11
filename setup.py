e730d412741d1447450eda0770b7aeb2ac5044b8
JT Archie <jarchie@pivotal.io>
2019-01-11 16:36:39 -0700

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+import json
+from setuptools import setup, find_packages
+
+# Load package.json contents
+with open("package.json") as data:
+    package = json.load(data)
+
+# Load list of dependencies
+with open("requirements.txt") as data:
+    install_requires = [
+        line for line in data.read().split("\n")
+            if line and not line.startswith("#")
+    ]
+
+# Package description
+setup(
+    name = "pivotal mkdocs theme",
+    version = "0.0.0",
+    license = "MIT",
+    author = "JT Archie",
+    author_email = "jarchie@pivotal.io",
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = install_requires,
+    entry_points = {
+        "mkdocs.themes": [
+            "pivotal = pivotal",
+        ]
+    },
+    zip_safe = False
+)
