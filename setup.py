0a9119f66285665542b57fe5d1de9f10b1c5a59d
JT Archie <jarchie@pivotal.io>
2019-01-11 16:38:26 -0700

    fix ref to package.json

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import json
 from setuptools import setup, find_packages
 
-# Load package.json contents
-with open("package.json") as data:
-    package = json.load(data)
-
 # Load list of dependencies
 with open("requirements.txt") as data:
     install_requires = [
